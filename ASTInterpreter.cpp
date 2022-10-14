6dd110ccd7b10e265afb5132aebf0777eb416e35
crossoverthere <1433065523@qq.com>
2022-10-14 16:01:34 +0800

    通过test03.c，实现了if操作与二元比较，并对代码进行了注释

--- ASTInterpreter.cpp
+++ ASTInterpreter.cpp
@@ -11,66 +11,111 @@ using namespace clang;
 
 #include "Environment.h"
 
-class InterpreterVisitor : 
-   public EvaluatedExprVisitor<InterpreterVisitor> {
+// 提取AST节点信息
+// 需要编写这一部分实现RecursiveASTVisitor
+// 仅遍历stmt节点，无Decl节点
+class InterpreterVisitor : public EvaluatedExprVisitor<InterpreterVisitor>
+{
 public:
-   explicit InterpreterVisitor(const ASTContext &context, Environment * env)
-   : EvaluatedExprVisitor(context), mEnv(env) {}
+   explicit InterpreterVisitor(const ASTContext &context, Environment *env)
+       : EvaluatedExprVisitor(context), mEnv(env) {}
    virtual ~InterpreterVisitor() {}
 
-   virtual void VisitBinaryOperator (BinaryOperator * bop) {
-	   VisitStmt(bop);
-	   mEnv->binop(bop);
+   virtual void VisitBinaryOperator(BinaryOperator *bop)
+   {
+      VisitStmt(bop);
+      mEnv->binop(bop);
    }
-   virtual void VisitDeclRefExpr(DeclRefExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->declref(expr);
+   virtual void VisitDeclRefExpr(DeclRefExpr *expr)
+   {
+      VisitStmt(expr);
+      mEnv->declref(expr);
    }
-   virtual void VisitCastExpr(CastExpr * expr) {
-	   VisitStmt(expr);
-	   mEnv->cast(expr);
+   virtual void VisitCastExpr(CastExpr *expr)
+   {
+      VisitStmt(expr);
+      mEnv->cast(expr);
    }
-   virtual void VisitCallExpr(CallExpr * call) {
-	   VisitStmt(call);
-	   mEnv->call(call);
+   virtual void VisitCallExpr(CallExpr *call)
+   {
+      VisitStmt(call);
+      mEnv->call(call);
    }
-   virtual void VisitDeclStmt(DeclStmt * declstmt) {
-	   mEnv->decl(declstmt);
+   virtual void VisitDeclStmt(DeclStmt *declstmt)
+   {
+      VisitStmt(declstmt);
+      mEnv->decl(declstmt);
    }
+   /*补充代码，用于访问其他类型节点*/
+   // IntegerLiteral节点
+   virtual void VisitIntegerLiteral(IntegerLiteral *literal)
+   {
+      VisitStmt(literal);
+      mEnv->literal(literal);
+   }
+   // IfStmt节点
+   virtual void VisitIfStmt(IfStmt *ifstmt)
+   {
+      // 不可直接取出ifstmt所有子结点
+      // 先取出判断节点
+      Expr *cond = ifstmt->getCond();
+      Visit(cond);
+      // 创建一个函数取出栈帧内保存的二元操作结果
+      if(mEnv->getValue(cond))
+      {
+         VisitStmt(ifstmt->getThen());
+      }
+      else
+      {
+         VisitStmt(ifstmt->getElse());
+      }
+   }
+   /**/
+
 private:
-   Environment * mEnv;
+   Environment *mEnv;
 };
 
-class InterpreterConsumer : public ASTConsumer {
+// 提供入口点，此处仅需要HandleTranslationUnit
+class InterpreterConsumer : public ASTConsumer
+{
 public:
-   explicit InterpreterConsumer(const ASTContext& context) : mEnv(),
-   	   mVisitor(context, &mEnv) {
+   explicit InterpreterConsumer(const ASTContext &context) : mEnv(),
+                                                             mVisitor(context, &mEnv)
+   {
    }
    virtual ~InterpreterConsumer() {}
 
-   virtual void HandleTranslationUnit(clang::ASTContext &Context) {
-	   TranslationUnitDecl * decl = Context.getTranslationUnitDecl();
-	   mEnv.init(decl);
+   virtual void HandleTranslationUnit(clang::ASTContext &Context)
+   {
+      TranslationUnitDecl *decl = Context.getTranslationUnitDecl();
+      mEnv.init(decl);
 
-	   FunctionDecl * entry = mEnv.getEntry();
-	   mVisitor.VisitStmt(entry->getBody());
-  }
+      FunctionDecl *entry = mEnv.getEntry();
+      mVisitor.VisitStmt(entry->getBody());
+   }
+
 private:
    Environment mEnv;
    InterpreterVisitor mVisitor;
 };
 
-class InterpreterClassAction : public ASTFrontendAction {
-public: 
-  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
-    return std::unique_ptr<clang::ASTConsumer>(
-        new InterpreterConsumer(Compiler.getASTContext()));
-  }
+// 入口，执行操作
+class InterpreterClassAction : public ASTFrontendAction
+{
+public:
+   virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+       clang::CompilerInstance &Compiler, llvm::StringRef InFile)
+   {
+      return std::unique_ptr<clang::ASTConsumer>(
+          new InterpreterConsumer(Compiler.getASTContext()));
+   }
 };
 
-int main (int argc, char ** argv) {
-   if (argc > 1) {
-       clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
+int main(int argc, char **argv)
+{
+   if (argc > 1)
+   {
+      clang::tooling::runToolOnCode(std::unique_ptr<clang::FrontendAction>(new InterpreterClassAction), argv[1]);
    }
 }
--- Environment.h
+++ Environment.h
@@ -11,37 +11,49 @@
 
 using namespace clang;
 
-class StackFrame {
-   /// StackFrame maps Variable Declaration to Value
-   /// Which are either integer or addresses (also represented using an Integer value)
-   std::map<Decl*, int> mVars;
-   std::map<Stmt*, int> mExprs;
-   /// The current stmt
-   Stmt * mPC;
-public:
-   StackFrame() : mVars(), mExprs(), mPC() {
-   }
+class StackFrame
+{
+	/// StackFrame maps Variable Declaration to Value 将变量声明映射为数值
+	/// Which are either integer or addresses (also represented using an Integer value) 变量或者地址(也可能常量)
+	std::map<Decl *, int> mVars;
+	std::map<Stmt *, int> mExprs;
+	/// The current stmt 当前stmt
+	Stmt *mPC;
 
-   void bindDecl(Decl* decl, int val) {
-      mVars[decl] = val;
-   }    
-   int getDeclVal(Decl * decl) {
-      assert (mVars.find(decl) != mVars.end());
-      return mVars.find(decl)->second;
-   }
-   void bindStmt(Stmt * stmt, int val) {
-	   mExprs[stmt] = val;
-   }
-   int getStmtVal(Stmt * stmt) {
-	   assert (mExprs.find(stmt) != mExprs.end());
-	   return mExprs[stmt];
-   }
-   void setPC(Stmt * stmt) {
-	   mPC = stmt;
-   }
-   Stmt * getPC() {
-	   return mPC;
-   }
+public:
+	StackFrame() : mVars(), mExprs(), mPC()
+	{
+	}
+
+	void bindDecl(Decl *decl, int val)
+	{
+		// 变量赋值
+		mVars[decl] = val;
+	}
+	int getDeclVal(Decl *decl)
+	{
+		assert(mVars.find(decl) != mVars.end());
+		return mVars.find(decl)->second;
+	}
+	void bindStmt(Stmt *stmt, int val)
+	{
+		// 操作结果
+		mExprs[stmt] = val;
+	}
+	int getStmtVal(Stmt *stmt)
+	{
+		// 获取栈帧中操作数
+		assert(mExprs.find(stmt) != mExprs.end());
+		return mExprs[stmt];
+	}
+	void setPC(Stmt *stmt)
+	{
+		mPC = stmt;
+	}
+	Stmt *getPC()
+	{
+		return mPC;
+	}
 };
 
 /// Heap maps address to a value
@@ -55,109 +67,170 @@ public:
 };
 */
 
-class Environment {
-   std::vector<StackFrame> mStack;
+class Environment
+{
+	std::vector<StackFrame> mStack; // 栈帧，用于保存节点序列？
 
-   FunctionDecl * mFree;				/// Declartions to the built-in functions
-   FunctionDecl * mMalloc;
-   FunctionDecl * mInput;
-   FunctionDecl * mOutput;
+	FunctionDecl *mFree; /// Declartions to the built-in functions
+	FunctionDecl *mMalloc;
+	FunctionDecl *mInput;
+	FunctionDecl *mOutput;
 
-   FunctionDecl * mEntry;
+	FunctionDecl *mEntry;
+
 public:
-   /// Get the declartions to the built-in functions
-   Environment() : mStack(), mFree(NULL), mMalloc(NULL), mInput(NULL), mOutput(NULL), mEntry(NULL) {
-   }
+	/// Get the declartions to the built-in functions
+	Environment() : mStack(), mFree(NULL), mMalloc(NULL), mInput(NULL), mOutput(NULL), mEntry(NULL)
+	{
+	}
 
+	/// Initialize the Environment
+	void init(TranslationUnitDecl *unit)
+	{
+		for (TranslationUnitDecl::decl_iterator i = unit->decls_begin(), e = unit->decls_end(); i != e; ++i)
+		{
+			if (FunctionDecl *fdecl = dyn_cast<FunctionDecl>(*i))
+			{
+				if (fdecl->getName().equals("FREE"))
+					mFree = fdecl;
+				else if (fdecl->getName().equals("MALLOC"))
+					mMalloc = fdecl;
+				else if (fdecl->getName().equals("GET"))
+					mInput = fdecl;
+				else if (fdecl->getName().equals("PRINT"))
+					mOutput = fdecl;
+				else if (fdecl->getName().equals("main"))
+					mEntry = fdecl;
+			}
+		}
+		mStack.push_back(StackFrame());
+	}
 
-   /// Initialize the Environment
-   void init(TranslationUnitDecl * unit) {
-	   for (TranslationUnitDecl::decl_iterator i =unit->decls_begin(), e = unit->decls_end(); i != e; ++ i) {
-		   if (FunctionDecl * fdecl = dyn_cast<FunctionDecl>(*i) ) {
-			   if (fdecl->getName().equals("FREE")) mFree = fdecl;
-			   else if (fdecl->getName().equals("MALLOC")) mMalloc = fdecl;
-			   else if (fdecl->getName().equals("GET")) mInput = fdecl;
-			   else if (fdecl->getName().equals("PRINT")) mOutput = fdecl;
-			   else if (fdecl->getName().equals("main")) mEntry = fdecl;
-		   }
-	   }
-	   mStack.push_back(StackFrame());
-   }
+	FunctionDecl *getEntry()
+	{
+		return mEntry;
+	}
 
-   FunctionDecl * getEntry() {
-	   return mEntry;
-   }
+	/*添加代码，用于实现节点访问细节操作*/
+	int getValue(Expr *expr)
+	{
+		// 此函数用于为外部返回栈帧内部相关变量
+		return mStack.back().getStmtVal(expr);
+	}
+	void literal(IntegerLiteral *literal)
+	{
+		// 将常量存入栈帧
+		int val = literal->getValue().getSExtValue();
+		mStack.back().bindStmt(literal, val);
+	}
+	/**/
 
-   // Get a value
-   int getExprValue(Expr *expr){
-		if(IntegerLiteral *literal = dyn_cast<IntegerLiteral>(expr))
-			return literal->getValue().getSExtValue();
+	/// !TODO Support comparison operation
+	void binop(BinaryOperator *bop)
+	{
+		Expr *left = bop->getLHS();
+		Expr *right = bop->getRHS();
+
+		if (bop->isAssignmentOp())
+		{
+			int val = mStack.back().getStmtVal(right);
+			mStack.back().bindStmt(left, val);
+			if (DeclRefExpr *declexpr = dyn_cast<DeclRefExpr>(left))
+			{
+				// 为变量赋予新值
+				Decl *decl = declexpr->getFoundDecl();
+				mStack.back().bindDecl(decl, val);
+			}
+		}
 		else
-			return mStack.back().getStmtVal(expr);
-   }
+		{	
+			// 其余二元操作实现
+			clang::BinaryOperator::Opcode op = bop->getOpcode();
+			int left_value = mStack.back().getStmtVal(left);
+			int right_value = mStack.back().getStmtVal(right);
+			int result;
+			switch (op) {
+				default:
 
-   /// !TODO Support comparison operation
-   void binop(BinaryOperator *bop) {
-	   Expr * left = bop->getLHS();
-	   Expr * right = bop->getRHS();
+				case BO_EQ: 
+					result = left_value == right_value;
+					break;
+			}
+			// 将二元操作结果存入栈帧
+			mStack.back().bindStmt(bop, result);
+		}
+	}
 
-	   if (bop->isAssignmentOp()) {
-		   //int val = mStack.back().getStmtVal(right);
-		   int val = getExprValue(right);
-		   mStack.back().bindStmt(left, val);
-		   if (DeclRefExpr * declexpr = dyn_cast<DeclRefExpr>(left)) {
-			   Decl * decl = declexpr->getFoundDecl();
-			   mStack.back().bindDecl(decl, val);
-		   }
-	   }
-   }
+	void decl(DeclStmt *declstmt)
+	{
+		// 声明操作
+		for (DeclStmt::decl_iterator it = declstmt->decl_begin(), ie = declstmt->decl_end();
+			 it != ie; ++it)
+		{
+			Decl *decl = *it;
+			if (VarDecl *vardecl = dyn_cast<VarDecl>(decl))
+			{
+				if (vardecl->hasInit())
+				{
+					// 有初始化
+					int val = mStack.back().getStmtVal(vardecl->getInit());
+					mStack.back().bindDecl(vardecl, val);
+				}
+				else
+				{
+					// 无初始化，则置0
+					mStack.back().bindDecl(vardecl, 0);
+				}
+			}
+		}
+	}
 
-   void decl(DeclStmt * declstmt) {
-	   for (DeclStmt::decl_iterator it = declstmt->decl_begin(), ie = declstmt->decl_end();
-			   it != ie; ++ it) {
-		   Decl * decl = *it;
-		   if (VarDecl * vardecl = dyn_cast<VarDecl>(decl)) {
-			   mStack.back().bindDecl(vardecl, 0);
-		   }
-	   }
-   }
-   void declref(DeclRefExpr * declref) {
-	   mStack.back().setPC(declref);
-	   if (declref->getType()->isIntegerType()) {
-		   Decl* decl = declref->getFoundDecl();
+	void declref(DeclRefExpr *declref)
+	{
+		mStack.back().setPC(declref);
+		if (declref->getType()->isIntegerType())
+		{
+			Decl *decl = declref->getFoundDecl();
 
-		   int val = mStack.back().getDeclVal(decl);
-		   mStack.back().bindStmt(declref, val);
-	   }
-   }
+			int val = mStack.back().getDeclVal(decl);
+			// 将变量数值存入栈帧
+			mStack.back().bindStmt(declref, val);
+		}
+	}
 
-   void cast(CastExpr * castexpr) {
-	   mStack.back().setPC(castexpr);
-	   if (castexpr->getType()->isIntegerType()) {
-		   Expr * expr = castexpr->getSubExpr();
-		   int val = mStack.back().getStmtVal(expr);
-		   mStack.back().bindStmt(castexpr, val );
-	   }
-   }
+	void cast(CastExpr *castexpr)
+	{
+		mStack.back().setPC(castexpr);
+		if (castexpr->getType()->isIntegerType())
+		{
+			Expr *expr = castexpr->getSubExpr();
+			int val = mStack.back().getStmtVal(expr);
+			mStack.back().bindStmt(castexpr, val);
+		}
+	}
 
-   /// !TODO Support Function Call
-   void call(CallExpr * callexpr) {
-	   mStack.back().setPC(callexpr);
-	   int val = 0;
-	   FunctionDecl * callee = callexpr->getDirectCallee();
-	   if (callee == mInput) {
-		  llvm::errs() << "Please Input an Integer Value : ";
-		  scanf("%d", &val);
+	/// !TODO Support Function Call
+	void call(CallExpr *callexpr)
+	{
+		mStack.back().setPC(callexpr);
+		int val = 0;
+		FunctionDecl *callee = callexpr->getDirectCallee();
+		if (callee == mInput)
+		{
+			llvm::errs() << "Please Input an Integer Value : ";
+			scanf("%d", &val);
 
-		  mStack.back().bindStmt(callexpr, val);
-	   } else if (callee == mOutput) {
-		   Expr * decl = callexpr->getArg(0);
-		   val = mStack.back().getStmtVal(decl);
-		   llvm::errs() << val;
-	   } else {
-		   /// You could add your code here for Function call Return
-	   }
-   }
+			mStack.back().bindStmt(callexpr, val);
+		}
+		else if (callee == mOutput)
+		{
+			Expr *decl = callexpr->getArg(0);
+			val = mStack.back().getStmtVal(decl);
+			llvm::errs() << val;
+		}
+		else
+		{
+			/// You could add your code here for Function call Return
+		}
+	}
 };
-
-
